30c18603c3d9ceda0b89b399e1c5a2474a7daeab
Esra Yazar <esrayzr@gmail.com>
2022-03-14 00:01:34 -0400

    setup project

---
+++ mPulse/src/main/java/com/esra/mPulse/MPulseApplication.java
@@ -0,0 +1,13 @@
+package com.esra.mPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MPulseApplication.class, args);
+	}
+
+}
